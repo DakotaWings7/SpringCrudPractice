186f47e4a701d9cef0c4cc588263f46a2cb391a2
Djanik Aliyev <djanik_aliyev@mail.ru>
2024-04-07 13:14:57 +0300

    Now DB properties are in the file. Used @PropertySource to set property directory, used Environment class to access properties

--- src/main/java/ru/aliev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/aliev/springcourse/dao/PersonDAO.java
@@ -29,9 +29,8 @@ public class PersonDAO {
                 .stream().findAny().orElse(null);
     }
 
-
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(), person.getAge(),
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
                 person.getEmail());
     }
 
